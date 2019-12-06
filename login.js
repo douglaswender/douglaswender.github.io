486ffc5b0188e8ae171cf7aa93d2b14424d83771
Douglas <dglswender@gmail.com>
2019-12-06 15:53:19 -0300

    Atualizando página para um exemplo de login - VV

---
+++ login.js
@@ -0,0 +1,8 @@
+function login(user, pass){
+    console.log(window.location.href)
+    
+    if(user === 'admin' && pass === 'admin123'){
+        window.location.href = 'https://douglaswender.github.io/logged.html' 
+    }
+
+}
