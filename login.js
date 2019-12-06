d50314cf8ee9050aac263025c741e4c1bd67bf82
Douglas <dglswender@gmail.com>
2019-12-06 16:18:17 -0300

    att

--- login.js
+++ login.js
@@ -3,9 +3,7 @@ function login(user, pass){
     
     if(user === 'admin' && pass === 'admin123'){
         console.log('dentro');
-        setTimeout(()=>{
-            window.location.replace("https://douglaswender.github.io/logged.html");
-        }, 3000);
+        window.location.replace("https://douglaswender.github.io/logged.html");
     }
 
 }
