1a01cfe8c362bad83cdc5f5d3a1e3b84bd7ce9ca
Douglas Wender <dglswender@gmail.com>
2020-07-17 10:29:50 -0400

    corrigindo js fullHeight para determinados dispositivos móveis

--- login.js
+++
@@ -1,9 +0,0 @@
-function login(user, pass){
-    console.log(window.location.href)
-    
-    if(user === 'admin' && pass === 'admin123'){
-        console.log('dentro');
-        window.location.replace("https://douglaswender.github.io/logged.html");
-    }
-
-}
